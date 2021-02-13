c2f33cd4d440f25805316828aca75fe28b15626d
BuildTools <unconfigured@null.spigotmc.org>
2021-02-13 11:31:22 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='WolframAlphaDiscordBot',
+    version='1.1',
+    packages=['WADiscordBot'],
+    url='https://github.com/The-Bow-Hunter/WolframAlphaDiscordBot',
+    license='MIT',
+    author='Tom Mucke',
+    author_email='tom.mucke@web.de',
+    description='Easy to use and understand Wolfram Alpha Discord Bot using the Wolfram simple API',
+    install_requires=['discord.py',
+                      'requests'],
+    python_requires='>=3'
+)
