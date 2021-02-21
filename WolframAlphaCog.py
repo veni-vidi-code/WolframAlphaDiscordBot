ab6cbf95fc9367ccca683e237435fe26ba12453d
BuildTools <unconfigured@null.spigotmc.org>
2021-02-21 02:00:03 +0100

    Added an version as a cog aswell in order of making it easier to be used in bigger bot projects

---
+++ WolframAlphaCog.py
@@ -0,0 +1,57 @@
+from io import BytesIO
+from urllib import parse as urlparse
+
+import requests
+from discord import Embed, File
+from discord.ext import commands
+
+
+class WolframAlpha(commands.Cog, name="Server File Listener"):
+    def __init__(self, bot: commands.Bot, token: str):
+        """
+        Wolfram Alpha Cog
+        https://github.com/The-Bow-Hunter
+
+        Using the Wolfram Alpha simple Api
+
+        Remember to fill in a valid Token!
+        """
+        if token == "":
+            raise Exception("Missing you Wolfram Alpha Token! Please fill it out in the WolframAlphaCog")
+        self.b = bot
+        print("""
+        *********************************
+        Wolfram Alpha Cog added
+        https://github.com/The-Bow-Hunter
+        *********************************
+        """)
+
+        self.WABASICURL = str("http://api.wolframalpha.com/v1/simple?appid=" + token + "&i=")
+
+    @commands.command(name="WolframAlpha",
+                      help="Schreibe hinter den command deine Frage in Anführungszeichen (z.B. \"1+1\"), warte etwas "
+                           "und erhalte die Antwort von "
+                           "Wolfram Alpha. Schau doch mal auf [Github](https://github.com/The-Bow-Hunter)",
+                      brief="Returns a wolfram Alpha answer", aliases=["wa", "w|a", "wolfram"])
+    @commands.cooldown(2, 40, commands.BucketType.user)
+    async def walpha(self, ctx: commands.context, arg):
+        questionurl = str(self.WABASICURL + str(urlparse.quote(arg)))
+        embed = Embed(title="Wolfram|Alpha", url="https://wolframalpha.com/",
+                              description="Hier hast du deine Antwort von Wolfram Alpha")
+        embed.set_author(name="WolframAlphaBot by TM", url="https://github.com/The-Bow-Hunter")
+        embed.add_field(name="Github", value="https://github.com/The-Bow-Hunter", inline=False)
+        embed.set_footer(text="Not supported by Wolfram Alpha in any way. This is a (possibly modified) copy of the "
+                              "cog from https://github.com/The-Bow-Hunter")
+        r = requests.get(questionurl)
+        if r.content == "Error 1: Invalid appid":
+            await ctx.reply("Es scheint als wäre ich momentan in Wartung. Sorry")
+            print("Invalid appid error")
+        else:
+            await ctx.reply(file=File(fp=BytesIO(r.content), filename="WolframAlphaBot.gif"), embed=embed)
+        return
+
+    @walpha.error  # We do want to let people know why they do not get an answer
+    async def walpha_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.reply(f"You are sending this command to fast! Try again in {error.retry_after:.2f}s.",
+                            delete_after=15)
